6b0b13c2868c230385e0a513dc822ce2990f45ea
ndjo7189 <andrew.jo7189@gmail.com>
2016-05-04 07:25:45 -0400

    Added files via upload

---
+++ Using_Databases_With_Python/Assignment_13.py
@@ -0,0 +1,17 @@
+# Andrew Jo
+# Using Python to Access Web Data
+# Assignment 13
+# 05/03/2016
+# Retrieving GEOData
+
+# Download the code from http://www.pythonlearn.com/code/geodata.zip - then unzip 
+# the file and edit where.data to add an address nearby where you live - don't reveal 
+# your actual location. Then run the geoload.py to lookup all of the entries in where.data 
+# (including the new one) and produce the geodata.sqlite. Then run geodump.py to read the 
+# database and produce where.js. Then open where.html to visualize the map. Take screen 
+# shots as described below.
+
+# This is a relatively simple assignment. Don't take off points for little mistakes. 
+# If they seem to have done the assignment give them full credit. Feel free to make suggestions 
+# if there are small mistakes. Please keep your comments positive and useful. If you do not 
+# take grading seriously, the instructors may delete your response and you will lose points.
